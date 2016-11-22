6a05d23bd61af53db842eb8f89f89bb3e06be6a2
gdamron <grant@pullstring.com>
2016-11-22 15:53:01 -0800

    add license and documentation

--- src/VersionInfo.js
+++ src/VersionInfo.js
@@ -1,17 +1,41 @@
-var Features = {
+/*
+ * Encapsulates version information for PullString's' Web API.
+ *
+ * Copyright (c) 2016 PullString, Inc.
+ *
+ * The following source code is licensed under the MIT license.
+ * See the LICENSE file, or https://opensource.org/licenses/MIT.
+ */
+
+/**
+ * Define features to check if they are supported.
+ * @readonly
+ * @property {int} EFeatures.StreamingAsr
+ */
+const EFeatures = {
     StreamingAsr: 0,
 };
 
+/**
+ * Encapsulates version information for PullString's' Web API.
+ */
 class VersionInfo {
+    /**
+     * Check if the endpoint currently supports a feature.
+     * @param {EFeatures} feature The feature to check.
+     */
     static hasFeature(feature) {
         switch (feature) {
-        case Features.StreamingAsr:
+        case EFeatures.StreamingAsr:
         default:
             return false;
         }
     }
 }
 
+/**
+ * The public-facing endpoint of the PullString Web API.
+ */
 Object.defineProperty(VersionInfo, 'ApiBaseUrl', {
     value: 'https://conversation.pullstring.ai/v1/',
     writable: false,
@@ -19,6 +43,6 @@ Object.defineProperty(VersionInfo, 'ApiBaseUrl', {
     configurable: false,
 });
 
-VersionInfo.Features = Features;
+VersionInfo.EFeatures = EFeatures;
 
 module.exports = { VersionInfo };
